ef3049818b4571f5aaa874456438df5944b47175
voromahery <daniel.her@onja.org>
2020-10-09 15:30:50 +0300

    Added condition for the style

--- Buttons.js
+++ Buttons.js
@@ -2,11 +2,20 @@ import React from 'react'
 import './style.css';
 
 function Button(props) {
+    let style = ""; 
+if (props.color) {
+    style = props.color;
+} else if (props.variant) {
+    style = props.variant;
+} else if (props.size) {
+    style = props.size;
+} else if (props.color && props.size && props.variant) {
+    style = props.color;
+}
     return (
         <div>
             <div>
-                <p></p>
-                <button className={props.className}>Default</button>
+                <button className={style}>{props.children}</button>
             </div>
         </div>
 
--- index.js
+++ index.js
@@ -2,34 +2,38 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Button from './Buttons.js';
 import './style.css';
-
-
+import icon from './icon/shopping-cart.svg';
 
 function App() {
     return (
-        <div>
-            <Button />
-            <Button className="outline" />
-            <Button className="outline-text" />
-            <Button className="disableShadow" />
+        <div className="grid">
+            <Button>Default</Button>
+            <div className="outline-button">
+                <Button variant="outline">Default</Button>
+                <Button variant="outline-text">Default</Button>
+            </div>
+            <div>
+                <Button variant="disableShadow">Default</Button>
+            </div>
             <div className="disable">
-                <Button className="disable-button" />
-                <Button className="disable-text" />
+                <Button variant="disable-button">Disabled</Button>
+                <Button variant="disable-text">Disabled</Button>
             </div>
             <div className="icon">
-                <Button className="start-icon" />
-                <Button className="end-icon" />
+                <Button variant="start-icon"><img src={icon} alt="shopping-cart" /> Default</Button>
+                <Button variant="end-icon">Default <img src={icon} alt="shopping-cart" /></Button>
             </div>
             <div className="size">
-                <Button className="sm" />
-                <Button className="md" />
-                <Button className="lg" />
+                <Button size="sm">Default</Button>
+                <Button size="md">Default</Button>
+                <Button size="lg">Default</Button>
             </div>
             <div className="color">
-                <Button className="default" />
-                <Button className="primary" />
-                <Button className="secondary" />
-                <Button className="danger" />
+                <Button color="default">Default</Button>
+                <Button color="primary">Primary</Button>
+                <Button color="secondary">Secondary</Button>
+                <Button color="danger">Secondary</Button>
+                <Button size="lg" color="danger">Danger</Button>
             </div>
         </div>
     )
